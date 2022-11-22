6d08767b1b1449cb52ef8be5f1a7b88f9df4d611
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2022-11-22 12:59:00 +0530

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -65,3 +65,25 @@ contract banking
         return user_exists[msg.sender];
     }
 }
+
+
+####################################################################################################################################
+
+pragma solidity ^0.4.19;
+
+contract Bank {
+
+    mapping(address => uint256) public balanceOf;   // balances, indexed by addresses
+
+    function deposit(uint256 amount) public payable {
+        require(msg.value == amount);
+        balanceOf[msg.sender] += amount;     // adjust the account's balance
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount <= balanceOf[msg.sender]);
+        balanceOf[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+    }
+}
+
